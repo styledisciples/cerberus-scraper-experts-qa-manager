f7f727fe007009e89cc68edef3143ec34a95d4d9
mridulsri <mridul81@gmail.com>
2018-06-29 00:47:35 +0530

    Updated for project settings from environment.

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@
 module.exports = (function () {
     let settings = {
         "mongo": {
-            "uri": "mongodb://81.174.151.175:27017/hades_dev",
+            "uri": process.env.MONGO_CONNECTION_STRING, //"mongodb://81.174.151.175:27017/hades_dev",
             "experts": "experts"
         }
     };
